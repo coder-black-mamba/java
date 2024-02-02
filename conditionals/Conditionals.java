451b6c3549a8399a6a65be8036e5307eb6231445
coder-black-mamba <it.is.the.black.mamba@gmail.com>
2024-02-02 22:17:28 +0600

    conditionals_added

---
+++ conditionals/Conditionals.java
@@ -0,0 +1,44 @@
+// for using the folder conditionals as i am compiling it from root
+package conditionals;
+
+public class Conditionals {
+    public static void main(String args[]) {
+        // simple if statement
+        // Let's take an example of an adult site where 18 or greater aged people access
+        // whole less than less than eightteen but grater than fiften can access a few
+        // content
+        int age = 10;
+        // age =18;
+        String gender = "male";
+        gender = "female";
+        // age =19;
+        if (age >= 18) {
+            System.out.println("You Can Access This Website");
+        }
+        // if with else
+        if (age >= 18) {
+            System.out.println("You Can Access The Website");
+        } else {
+            System.out.println("Hey You Are To Small Too Access This Website");
+        }
+        // if with else if
+        if (age >=18){
+            System.out.println("Hey You Can Access This Website");
+        }else if (age>=15){
+            System.out.println("Hey You Can Access A Few Content Of This Website");
+        }else{
+            System.out.println("Hey You Are To Small Too Access This Website");
+        }
+
+        // lets say we want to check if this person male or female if the person is greater 18 or male then we want to give them access
+        // and as 15 aged group is accepting male and female both
+        if(age>=18 && gender == "male" ){
+            System.out.println("Male and over 18");
+        }else if(age>=15 && (gender=="male" || gender=="female")){
+            System.out.println("Female Male and over 15");
+        }else{
+            System.out.println("Nothing");
+        }
+
+    }
+}
---
+++ conditionals/SWcase.java
@@ -0,0 +1,36 @@
+package conditionals;
+
+public class SWcase {
+    public static void main(String args[]) {
+        // if a single value matters to multiple cases then we use switch case
+        int day = 89;
+
+        switch (day) {
+            case 0:
+                System.out.println("Sat");
+                break;
+            case 1:
+                System.out.println("Sun");
+                break;
+            case 2:
+                System.out.println("Mon");
+                break;
+            case 3:
+                System.out.println("Tue");
+                break;
+            case 4:
+                System.out.println("Wed");
+                break;
+            case 5:
+                System.out.println("Thu");
+                break;
+            case 6:
+                System.out.println("Fri");
+                break;
+
+            default:
+            System.out.println("Please Input Valid Number");
+                break;
+        }
+    }
+}
